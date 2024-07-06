4c6b10663d3dbd3efa701dc73b894cb1686e11ce
Nguyễn Hải Đăng <154570161+NhDangcode@users.noreply.github.com>
2024-07-06 22:34:48 +0700

    Update Helmet.js

--- frontend/doan_ver/src/userSide/components/Helmet/Helmet.js
+++ frontend/doan_ver/src/userSide/components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Helmet = (props) => {
-    document.title = "LaS - " + props.title;
+    document.title = "Website bán truyện - " + props.title;
   return (
     <div className="w-100">{props.children}</div>
   )
